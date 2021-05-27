499a1259a1c3a5f066b1f00040f4191408e90b48
IndraP24 <indrashis985@gmail.com>
2021-05-28 00:08:22 +0530

    Add predict method for model

---
+++ author_identifier/model/model.py
@@ -0,0 +1,22 @@
+from author_identifier.util import utils
+
+class model:
+    @staticmethod    
+    def predict(text, probabilities, log_prior):
+        """
+        Given the dialogue (as a string), and the conditional probabilities of words (dict) in our corpus along with the 
+        prior probability (float), tokenizes the dialogue and uses naive bayes to figure out which character is more likely
+        to have spoken this dialogue
+        """
+        tokens = utils.tokenize(text)
+        score = log_prior
+        for tk in tokens:
+            if tk in probabilities:
+                score += probabilities[tk]["lambda"]
+            else:
+                score += probabilities["xxunk"]["lambda"]
+        
+        if score >= 1:
+            return ("Iron Man", score)
+        
+        return ("Captain America", score)
